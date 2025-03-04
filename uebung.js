a053f0c55bd1eef4c8058a976ce00ec18b7bdea6
Dennis Angermann <dennis.angermann@tn.techstarter.de>
2025-03-04 11:50:54 +0100

    Update uebung.js

--- uebung.js
+++ uebung.js
@@ -74,7 +74,10 @@ if (my_Name === 'Lukas') {
 }   else {
     console.log('Wer bist Du?');
 }
-// Definiton mit 2 Variablen als Zahlen zur Fallunterscheidung
+// Fingerübung: 
+// Definiert 2 variablen (zahlen) a und b
+// Gebt mithilfe von if-else if-else aus, welche Größer ist oder ob sie
+// gleich groß sind
 let n1 = 16
 let n2 = 4
 if (n1 <= n2) {
@@ -83,7 +86,31 @@ if (n1 <= n2) {
     console.log('Gewinner ist Nummer 2');
 }
 
+// Fingerübung 2: 
+// Erstelle eine variable divisionResult
+// Schreibe in diese Variable das Ergebnis von a/b, aber nur,
+// wenn b nicht 0 ist. Sonst benutze console.error um einen Fehler auszugeben.
+let zahl1 = a
+let zahl2 = b
+let divisionResult = a/b
+if (zahl2 < 0) {
+    console.log(divisionResult)
+} else {
+    console.error('Mit 0 rechne ich nicht!');
+}
 
+// Fingerübung 3: 
+// Erstelle eine variable punkte
+// Die Bewertung läuft wie folgt:
+// Mehr als 90 Punkte: Ausgabe "Note 1"
+// Mehr als 80 Punkte: Ausgabe "Note 2"
+// Mehr als 70 Punkte: Ausgabe "Note 3"
+// Mehr als 60 Punkte: Ausgabe "Note 4"
+// Sonst: Ausgabe "Durchgefallen!"
+let punkte 
+if (<= 90) {
+    console.log('Note 1')
+} elif ()
 
 
 
